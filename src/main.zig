460242991dd6734747e59f274128e814efcc277d
Luc Alexander <lucskyalex@gmail.com>
2026-04-07 16:07:23 +0200

    everything except for showing something with all present evidence

--- src/main.zig
+++ src/main.zig
@@ -8,17 +8,21 @@ const Name = u8;
 
 const Claim = union(enum){
 	impl: struct {
-		left: *Proof,
-		right: *Proof
+		left: *Claim,
+		right: *Claim
 	},
 	in: struct {
-		left: *Proof,
-		right: *Proof
+		left: *Claim,
+		right: *Claim
 	},
-	not: *Proof,
+	not: *Claim,
 	state: State,
 	param: Param,
-	named: Name,
+	named: struct {
+		left: *Claim,
+		right: *Claim,
+		name: Name
+	},
 
 	pub fn eql(left: Claim, right: Claim) bool {
 		if (std.meta.activeTag(left) == std.meta.activeTag(right)){
@@ -41,7 +45,7 @@ const Claim = union(enum){
 				return left.param == right.param;
 			},
 			.named => {
-				return left.named == right.named;
+				return Claim.eql(left.named.left.*, right.named.left.*) and Claim.eql(left.named.right.*, right.named.right.*) and left.named.name == right.named.name;
 			}
 		}
 	}
@@ -54,20 +58,43 @@ const Arg = union(enum){
 
 const Definition = struct {
 	body: Buffer(Claim),
+	claim: Claim,
 	name: Name,
-	leftarg: Arg,
-	rightarg: Arg,
 	
+	pub fn init(name: Name, claim: Claim, body: Buffer(Claim)) Definition {
+		return Definition{
+			.name = name,
+			.claim = claim,
+			.body = body
+		};
+	}
+
+	pub fn contains(self: *Definition, claim: Claim, mind: *Mind) bool {
+		for (self.body.items) |constraint| {
+			if (constraint == .named){
+				for (mind.definitions.items) |def| {
+					if (def.name == constraint.named.name){
+						if (constraint.contains(claim, mind)){
+							return true;
+						}
+						break;
+					}
+				}
+			}
+			else{
+				if (Claim.eql(constraint, claim)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 	pub fn eql(left: Definition, right: Definition) bool {
 		if (left.name == right.name){
 			return true;
 		}
-		if ((left.leftarg == .state and right.leftarg != .state) or
-			(left.leftarg == .param and right.leftarg != .param)) {
-			return false;
-		}
-		if ((left.rightarg == .state and right.rightarg != .state) or
-			(left.rightarg == .param and right.rightarg != .param)) {
+		if (Claim.eql(left.claim, right.claim) == false){
 			return false;
 		}
 		if (left.body.items.len != right.body.items.len){
@@ -85,12 +112,139 @@ const Definition = struct {
 const Mind = struct {
 	definitions: Set(Definition),
 	proofs: Set(Definition),
+	mem: *const std.mem.Allocator,
 
 	pub fn init(mem: *const std.mem.Allocator) Mind {
 		return Mind{
-			.definitions = Set(Definition).init(mem),
+			.mem = mem,
+			.definitions = Set(Definition).init(mem)
 			.proofs = Set(Definition).init(mem)
 		};
+	}
+
+	pub fn prove(self: *Mind, def: *Definition, local_scope: *Set(Claim).init(self.mem.*), argmap: *std.AutoHashMap(Param, Claim)) bool {
+		for (self.proofs.items) |proof| {
+			local_scope.put(proof.claim);
+		}
+		var i: u64 = 0;
+		while (i < def.body.items.len){
+			const constraint = &def.body.items[i];
+			if (self.prove_target(def, argmap, constraint, &local_scope) == false){
+				return false;
+			}
+			i += 1;
+		}
+		if (self.shows(def, argmap, def.claim, &local_scope) == false) {
+			return false;
+		}
+	}
+	//TODO at call: self.proofs.put(def);
+
+	pub fn prove_target(self: *Mind, def: Definition, argmap: *std.AutoHashMap(Param, Claim), proof: *Claim, local_scope: *Set(Claim)) bool {
+		switch (proof.*){
+			.impl => {
+				return self.shows(def, argmap, proof, local_scope);
+			},
+			.in => {
+				if (self.get_definition(proof.in.right)) |definition| {
+					var copy = definition.copy();
+					var newmap = std.AutoHashMap(Param, Claim).init(self.mem.*);
+					var newscope = Set(Claim).init(self.mem);
+					if (self.apply_argmap(&newmap, copy.claim, proof.in.right) == false) {
+						return false;
+					}
+					if (self.prove(&copy, newscope, newmap) == false){
+						return false;
+					}
+					return definition.contains(proof.in.left, self);
+				}
+				return false;
+			},
+			.not => {
+				return !self.prove_target(def, argmap, proof.not, local_scope);
+			},
+			.state => {
+				return self.shows(def, argmap, proof.state, local_scope);
+			},
+			.param => {
+				if (argmap.get(proof.param)) |claim| {
+					proof.* = claim;
+					return self.prove_target(def, argmap, proof, local_scope);
+				}
+				return false;
+			},
+			.named => {
+				for (self.definitions.items) |definition| {
+					if (definition.name == self.named.name){
+						var copy = definition.copy();
+						var newmap = std.AutoHashMap(Param, Claim).init(self.mem.*);
+						var newscope = Set(Claim).init(self.mem);
+						if (self.apply_argmap(&newmap, copy.claim, proof.in.right) == false){
+							return false;
+						}
+						if (self.prove(&copy, newscope, newmap) == false){
+							return false;
+						}
+						local_scope.put(copy.claim);
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+		return false;
+	}
+
+	pub fn shows(self: *Mind, def: Definition. argmap: *std.AutoHashMap(Param, Claim), claim: *Claim, local_scope: *Buffer(Claim)) bool {
+		for ()
+	}
+
+	pub fn apply_argmap(argmap: *std.AutoHashMap(Param, Claim), defclaim: Claim, claim: Claim) bool {
+		if (std.meta.activeTag(defclaim) != std.meta.activeTag(claim)){
+			if (defclaim == .param){
+				argmap.put(defclaim.param, claim) catch unreachable;
+				return true;
+			}
+		}
+		if (claim == .param){
+			return false;
+		}
+		switch (claim){
+			.impl => {
+				return apply_argmap(argmap, defclaim.impl.left, claim.impl.left) and apply_argmap(argmap, defclaim.impl.right, claim.impl.right);
+			},
+			.in => {
+				return apply_argmap(argmap, defclaim.in.left, claim.in.left) and apply_argmap(argmap, defclaim.in.right, claim.in.right);
+			},
+			.not => {
+				return apply_argmap(argmap, defclaim.not, claim.not);
+			},
+			.state => {
+				return true;
+			},
+			.named => {
+				if (defclaim.named.name != claim.named.name) {
+					return false;
+				}
+				return apply_argmap(argmap, defclaim.named.left, claim.named.left) and apply_argmap(argmap, defclaim.named.right, claim.named.right);
+			}
+			else => {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	pub fn get_definition(self: *Mind, claim: Claim) ?Definition {
+		if (claim != .named){
+			return null;
+		}
+		for (self.definitions.items) |def| {
+			if (def.name == claim.named.name){
+				return def;
+			}
+		}
+		return null;
 	}
 };
 
